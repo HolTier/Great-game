30ea661fced09f2791e4c553f1522f8da5b5f58e
Marcin Holcman <holcmanmarcin@gmail.com>
2022-08-17 01:41:37 +0200

    Create UserControler

---
+++ Great-game API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Great_game_API.DbModels;
+
+namespace Great_game_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly GreatGameDataContext _context;
+
+        public UserController(GreatGameDataContext context)
+        {
+            _context = context;
+        }
+    }
+}
